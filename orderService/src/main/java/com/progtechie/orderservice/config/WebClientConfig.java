54dd9ac938fda16b60c11725eedbe2522a4f5fe7
Tamara556 <murazyan.tamara5566@gmail.com>
2025-06-29 18:18:49 +0400

    changed WebClient into builder and LoadBalanced

--- orderService/src/main/java/com/progtechie/orderservice/config/WebClientConfig.java
+++ orderService/src/main/java/com/progtechie/orderservice/config/WebClientConfig.java
@@ -8,9 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
+
 }
