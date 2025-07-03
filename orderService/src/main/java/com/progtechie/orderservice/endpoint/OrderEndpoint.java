4bf12d5fe1c52da877e88792cdf72a0f513266da
Tamara556 <murazyan.tamara5566@gmail.com>
2025-07-03 13:37:28 +0400

    modified

--- orderService/src/main/java/com/progtechie/orderservice/endpoint/OrderEndpoint.java
+++ orderService/src/main/java/com/progtechie/orderservice/endpoint/OrderEndpoint.java
@@ -2,23 +2,36 @@ package com.progtechie.orderservice.endpoint;
 
 import com.progtechie.orderservice.dto.OrderRequest;
 import com.progtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderEndpoint {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory-service")
+    @Retry(name = "inventory-service")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
         return orderService.placeOrder(orderRequest);
     }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
+    }
 }
-
-
